b71d2fc81ebba89e202bf3a541432872cc6ccb0b
Almog Levy <55050577+AlmogLDev@users.noreply.github.com>
2021-04-29 19:40:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct node_t {
+    int x;
+    struct node_t *next;
+} *Node;
+
+typedef enum {
+    SUCCESS=0,
+    MEMORY_ERROR,
+    EMPTY_LIST,
+    UNSORTED_LIST,
+    NULL_ARGUMENT,
+} ErrorCode;
+
+
+int getListLength(Node list);
+bool isListSorted(Node list);
+Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code){
+
+
+}
+
+
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
